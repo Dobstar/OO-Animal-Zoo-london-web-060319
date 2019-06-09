5a723d9032d7d4afee2d2e9775ed9deea153e4ad
github username <github email address>
2019-06-09 21:39:29 +0100

    Some methods left to do still

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,26 @@
 class Animal
 
+    attr_reader :species, :nickname
+    attr_accessor :weight
+    
+    @@all = []
+
+    def initialize(nickname, weight, species)
+        @nickname = nickname
+        @weight = weight
+        @species = species
+        @@all << self
+    end 
+
+    def self.all
+        @@all
+    end 
+
+    def animals
+        Zoo.all.select {|zoo| zoo.animal == self}
+    end 
+
+    def self.find_by_species(species)
+        Zoo.all.select {|zoo| zoo.animal.species == species}
+    end 
 end
--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,34 @@
 class Zoo
 
+    @@all = []
+
+    def initialize(name, location)
+        @name = name
+        @location = location
+        @@all << self 
+    end 
+
+    def self.all
+        @@all 
+    end 
+
+    def animals
+        Zoo.all.select {|zoo| zoo.animal == self}
+    end 
+    
+    def animal_species
+        animal.map {|a| a.species}.uniq
+     end 
+
+     def animal_nicknames
+        animal.map {|a| a.nickname}
+    end 
+ 
+     def find_by_species(species)
+        animal.all.find do |a| a.species.name == species
+     end 
+
+     def find_by_location(location)
+
+     end 
 end
